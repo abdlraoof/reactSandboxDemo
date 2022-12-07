7f629a021ca319ee0af1b5286779612073e6f113
abdlraoof <84389426+abdlraoof@users.noreply.github.com>
2022-12-08 00:24:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1> Hello the, how are you doing? </h1>;
+ReactDOM.render(element, document.getElementById("root"));
